59ebc7e26a01a39d7584f4646c8436d28a302cc3
Fede Conte Grand <fede.contegrand@gmail.com>
2021-11-30 15:56:06 -0300

    landig responsive

--- client/src/views/LandingPage.jsx
+++ client/src/views/LandingPage.jsx
@@ -7,15 +7,15 @@ export default function LandingPage() {
     return (
         <div className="container">
             <div className="banner">
-               <h2>Hey Dani 👋</h2>
-               <p>With this app Im going to change your mind. Here you will be able to search all the great and
+               <h2 className="landingTitle">Hey Dani 👋</h2>
+               <p  className="landingText">With this app Im going to change your mind. Here you will be able to search all the great and
                    funny characters of the amazing Rick & Morty show.
                 </p> 
                 <ul>
-                    <li> <p>Search by location, episode, or see them all.</p></li>
-                    <li><p>Filter according gender, name, status, episodes, dimension and more.</p></li>
-                    <li><p>Dont worry about your internet speed, once you see the characters, they are stored
-                    on the browser.</p></li>
+                    <li> <p className="landingText">Search by location, episode, or see them all.</p></li>
+                    <li><p className="landingText">Filter according gender, name, status, episodes, dimension and more.</p></li>
+                    <li><p className="landingText">Dont worry about your internet speed, once you see the characters, they are stored
+                    on your browser.</p></li>
                 </ul>  
                <button onClick={()=>navigate("/home")}>Enter</button>
             </div>            
